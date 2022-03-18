a155b4c6fdf476ded59927bd2e521bdd43580347
icpove <gaoajia@qq.com>
2022-03-18 19:53:51 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+fi
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+
+cd -
+rm -rf docs/.vuepress/dist
---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { text: 'Home', link: '/' },
+  { text: 'Blog', link: "/"},
+  { text: 'wiki', link: "/pages/c7906c/"},
+  { text: 'Mirrors', link: "/"},
+  { text: 'About', link: "/pages/about/"},
+]
