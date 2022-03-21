1b9294637ac60d2b3c837cd162d307f01a0637cc
icpove <gaoajia@qq.com>
2022-03-21 15:05:17 +0800

    github actions

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,27 @@
 set -e
 
 # 生成静态文件
-npm run build
+yarn build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+if [ -z "$GUIDE_GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl1=git@github.com:lit-lug/LuoLi-Guide-wiki.git
+  githubUrl2=git@github.com:gaoajia/gh-pages-guode-docs.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl1=https://lit-lug:${GUIDE_GITHUB_TOKEN}@github.com/lit-lug/LuoLi-Guide-wiki.git 
+  githubUrl2=https://gaoajia:${GUIDE_GITHUB_TOKEN}@github.com/gaoajia/gh-pages-guode-docs.git
+  git config --global user.name "gaoajia"
+  git config --global user.email "gaoajia@qq.com"
+fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl1 master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl2 master # 推送到 gaoajia master 分支，用于部署腾讯CND
 
 cd -
 rm -rf docs/.vuepress/dist
