1ea7b7f08727d656856a0d16acad201478fce7af
icpove <gaoajia@qq.com>
2022-03-22 23:39:14 +0800

    Update necessary configuration files

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ const feed_options = {
 };
 
 module.exports = {
-  title: "LUG @ LIT",
+  title: "洛理指南",
   description: 'Luoyang Institute of Science and Technology Linux User Group',
   base, // '/<仓库名>/'， 默认'/'
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
@@ -27,25 +27,25 @@ module.exports = {
   theme: require.resolve('../../theme-vdoing'), // 使用本地主题
   themeConfig: { // 主题配置
     nav,
-    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    sidebarDepth: 3, // 侧边栏显示深度，默认1，最大2（显示到h5标题）
     logo: 'https://image.gaoajia.com/2021/10/28/d7b19b2e89822.png', // 导航栏logo
     repo: 'lit-lug/LuoLi-Guide-wiki', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
-    lastUpdated: 'Last Update', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+    lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
-    // docsDir: 'docs', // 编辑的文件夹
-    // editLinks: true, // 编辑链接
-    // editLinkText: '编辑',
+    docsDir: 'docs', // 编辑的文件夹
+    editLinks: true, // 编辑链接
+    editLinkText: '编辑',
 
     // 以下配置是Vdoing主题改动的和新增的配置
     sidebar: { mode: 'structuring', collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
 
-    // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
+    sidebarOpen: true, // 初始状态是否打开侧边栏，在侧边栏关闭状态下，页面向下滚动时会隐藏顶部导航栏，让用户更专注于阅读。可以通过 front matter 来禁用指定页面的侧边栏。默认禁用。
     updateBar: { // 最近更新栏
-      showToArticle: false, // 显示到文章页底部，默认true
-      // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+      showToArticle: true, // 显示到文章页底部，默认true
+      moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
     },
-    // titleBadge: false, // 文章标题前的图标是否显示，默认true
+    titleBadge: true, // 文章标题前的图标是否显示，默认true
     // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
     //   '图标地址1',
     //   '图标地址2'
@@ -55,18 +55,16 @@ module.exports = {
     //   'https://image.iluoli.ren/2021/11/17/5a892c74a2112.jpg',
     // ], // body背景大图，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
     // bodyBgImgOpacity: 0.7, // body背景图透明度，选值 0 ~ 1.0, 默认0.5
-
     // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
 
-    // contentBgStyle: 1,
-
+    contentBgStyle: 6, // 文章内容块的背景底纹，选值：1 => 方格 | 2 => 横线 | 3 => 竖线 | 4 => 左斜线 | 5 => 右斜线 | 6 => 点状
     category: true, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
     tag: true, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
-     archive: true, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
+    archive: true, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
 
     author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
-      name: 'LUG', // 必需
-      href: 'https://www.litunix.org' // 可选的
+      name: 'AjiaErin', // 必需
+      href: 'https://www.gaoajia.com' // 可选的
     },
     social:{ // 社交图标，显示于博主信息栏和页脚栏
       // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
@@ -95,21 +93,19 @@ module.exports = {
     htmlModules,
   },
   plugins: [
-    ['vuepress-plugin-simple-analytics'],
-    [ 'feed', feed_options ],
     ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
       thirdparty: [ // 可选，默认 []
         {
-          title: 'Search in Google',
+          title: '在 Google 搜索 ',
           frontUrl: 'https://www.google.com/search?q=', // 搜索链接的前面部分
           behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
         },
         {
-          title: 'Search in Baidu',
+          title: '在 Baidu 搜索',
           frontUrl: 'https://www.baidu.com/s?ie=UTF-8&wd=',
         },
         {
-          title: 'Search in Bing',
+          title: '在 Bing 搜索',
           frontUrl: 'https://cn.bing.com/search?q='
         }
       ]
